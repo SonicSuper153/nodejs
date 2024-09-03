569b6df7dccf51a843e6acd7ac15771c636d8ed0
SonicSuper153 <ishmeetsinghnagi@gmail.com>
2024-09-03 07:46:56 +0530

    third

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const PORT = process.env.PORT;
 const server = http.createServer((req, res) => { 
     // res.setHeader('Content-Type','text/html');
     // res.statueCode = 404;
-    console.log(req.url);
-    console.log(req.method);
+    
     res.writeHead(200, { 'Content-Type': 'text/html'})
     res.end('<h1>vdfgh</h1>');
 });
